d601b110affa5d0cb96e37434d0899364f7ec1ce
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-05 20:50:23 -0300

    inicializando estudos

---
+++ aula3/index.js
@@ -0,0 +1,4 @@
+console.log("'Johny Carvalho'"); //string = texto
+console.log('Johny "Carvalho"'); //Number = Número
+console.log(`'Johny Carvalho'`);
+
---
+++ ex/inde.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Johny". Estou aprendendo JavaScript às', 20.43 , 'da noite.')
