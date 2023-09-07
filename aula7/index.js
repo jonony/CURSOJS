79beeb9bebfdd444e62a84ba8c9f91ad2ed08126
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-06 21:13:47 -0300

    novos exs

---
+++ aula7/index.js
@@ -0,0 +1,5 @@
+const primeiroNumero = 5
+const segundoNumero = 10
+const conta = primeiroNumero * segundoNumero
+const resultado = conta * 2
+console.log(resultado)
---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Johny alaf'
+const sobrenome = 'Carvalho'
+const idade = 21
+const peso = 75
+const altura = 1.80
+let imc = peso / (altura * altura) 
+let anoNascimento = 2023 - idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${altura} de altura e seu IMC é de ${imc}. ${nome} ${sobrenome} nasceu em ${anoNascimento}`)
+
