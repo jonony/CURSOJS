2ae4268d1fb1cd4b9699b4bab85ce9ba3d9f5539
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-09 14:44:01 -0300

    criei aula 16

---
+++ aula16/index.js
@@ -0,0 +1,16 @@
+const alunos = ['Luiz', 'Maria', 'joao']
+
+alunos.push('pedro')
+alunos.push('eduarda')
+
+console.log(alunos.slice(0,-1))
+
+//const removido = alunos.shift()
+
+//console.log(removido)
+//console.log(alunos)
+
+
+//alunos[0] = 'Eduardo'
+//[3] = 'luiza'
+//console.log(alunos[2])
