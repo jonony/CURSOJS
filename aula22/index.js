5ed9f80794223eb6b6e4a1d89f784f845997e6b3
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-14 14:21:01 -0300

    Operadores Lógicos

---
+++ aula22/index.js
@@ -0,0 +1,15 @@
+/*  Operadores lógicos
+    
+    &&   -> AND -> E
+    
+    ||   -> OR  -> OU
+
+    !    -> NOT -> NÃO
+*/
+
+const usuário = 'luiz'
+const senha = '123456'
+
+const vaiLogar = usuário === 'luiz' && senha === '123456'
+
+console.log(vaiLogar)
